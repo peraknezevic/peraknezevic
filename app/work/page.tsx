5430df7a5bd10df599b892642faba667633e6687
peraknezevic <predragknezevic@icloud.com>
2024-08-22 15:55:00 +0200

    tpes, work, cards updated

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,18 +1,32 @@
-import Card from "@/components/Card"
-import { Website } from "../types"
+import { Book, Website } from "../types"
+
+import BookCard from "@/components/BookCard"
+import WebCard from "@/components/WebCard"
 import { getLocalData } from "@/utils/actions"
 
 const Work = async () => {
   const websites = await getLocalData("websites")
+  const books = await getLocalData("books")
 
   return (
     <div className="max-w-screen-xl flex flex-col gap-4 mx-auto">
-      <h2 className="text-4xl xl:text-5xl font-bold pb-16 text-slate-900 dark:text-slate-100 ">
+      <h2 className="text-4xl xl:text-5xl font-bold pb-8 text-slate-900 dark:text-slate-100 ">
         Work
       </h2>
-      <div className="flex flex-col dark:border-slate-100 border-t-2 border-slate-900">
+      <h3 className="text-2xl font-semibold text-slate-900 dark:text-slate-100 pb-4 mb-8 border-b-2 dark:border-slate-100 border-black">
+        Web Design &amp; Development
+      </h3>
+      <div className="flex flex-col">
         {websites.map((website: Website) => {
-          return <Card website={website} key={website.id} />
+          return <WebCard website={website} key={website.id} />
+        })}
+      </div>
+      <h3 className="text-2xl font-semibold text-slate-900 dark:text-slate-100 pb-4 mb-8 border-b-2 dark:border-slate-100 border-black">
+        Book Cover Designs
+      </h3>
+      <div className="flex flex-col">
+        {books.map((book: Book) => {
+          return <BookCard book={book} key={book.id} />
         })}
       </div>
     </div>
